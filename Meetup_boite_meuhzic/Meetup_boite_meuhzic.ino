dd37efc155dedfec3ca519cd5b5996c04aca9d75
Ducord Florian <florian.developpement@gmail.com>
2020-10-29 21:07:18 +0100

    🐛 correction lecture nombre track

--- Meetup_boite_meuhzic/Meetup_boite_meuhzic.ino
+++ Meetup_boite_meuhzic/Meetup_boite_meuhzic.ino
@@ -28,7 +28,7 @@ void setup()
   pinMode(switchPin, INPUT);
   digitalWrite(switchPin, HIGH);
 
-  delay(50); //attente fin init myMP3.begin(mySerial)
+  delay(1000); //attente fin init myMP3.begin(mySerial)
   nbTrack = myMP3.numSdTracks();
   Serial.print("nb track: ");
   Serial.println(nbTrack);
